5d4e64c0e6d5bfd01d484bde88edfbed70543e1b
hyurai <yuumanagai0504@icloud.com>
2021-09-17 15:47:20 +0000

    edit_sign_up_redirect_

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -53,7 +53,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # The path used after sign up.
    def after_sign_up_path_for(resource)
      Profile.create(user_id: current_user.id)
-     super(resource)
+     tweets_path()
    end
 
   # The path used after sign up for inactive accounts.
