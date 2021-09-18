0aba9f0863f7d5284d6171577599fafbe0706200
hyurai <yuumanagai0504@icloud.com>
2021-09-18 06:49:56 +0000

    flex 写真nilにならないようにした

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,7 @@ class Profile < ApplicationRecord
     belongs_to :user
     mount_uploader :front_img, ImageUploader
     mount_uploader :back_img, ImageUploader
+    
+    validates :front_img,presence: true
+    validates :back_img,presence: true
 end
